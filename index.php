8891ed0340d8089454af4ef588362315e51607d2
N4N001 <N4N001@protonmail.com>
2021-03-30 10:54:30 +0200

    přidání index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    $money = 1000;
+
+    if($money >= 859) {
+        echo "Máš peníze na Fiziho golden money gun.";
+    } elseif($money <= 0) {
+        echo "Nemáš peníze, na co chceš money gun?"
+    } else {
+        echo "Nemáš dost peněz."
+
+</body>
+</html>
